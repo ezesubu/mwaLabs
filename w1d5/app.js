4af8dc424bab5f953708c1f849c6189d09cfd396
Ezequiel Suarez <ezesubu@gmail.com>
2018-10-05 15:17:15 -0500

    w1d5 - express homework

---
+++ w1d5/app.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const { of } = require("rxjs");
+const app = express();
+
+app.listen(3000, (e)=> console.log('Server Runing...'))
+app.get('/users', function(req,res){
+   let url = 'http://jsonplaceholder.typicode.com/users/';
+   let promise = fetch(url);
+   //promisses
+   promise.then(data => data.json()).then(json => res.json(json));
+   //observers
+   of(promise).subscribe((e)=> console.log("el dato", e.then(data => data.json()).then(json => res.json(json))))
+   //Away , async fucntion
+   (async function () {
+        const data = await fetch(url);
+        const json = await data.json();
+        res.json(json)
+    })();
+
+})
