d7f5a6cca03ca7f5e57cd67a0f6ec598bee1fe91
Ezequiel Suarez <ezesubu@gmail.com>
2018-10-11 09:59:53 -0500

    update queriess

--- w2d3/exe.js
+++ w2d3/exe.js
@@ -9,11 +9,13 @@ R. db.zips.aggregate({$match: {population: {$min: 10000}}},{$group: {_id: "0",zi
 R.  db.zips.aggregate({$match:{pop: {$lt:1000}}},{$group: {_id: "0",zip: {$addToSet: "$_id"}}})
 
 3.Find all cities that have more than one zip code, sort the results based by
-R.  state and city name.
+ state and city name.
 
-R. db.zips.aggregate({$group:{_id: "$city", zip_codes:{$addToSet: "$_id"}}},{$match:{zip_codes:{$size: 2}}})
+R.db.zips.aggregate({$group:{_id: "$city", state:{$first: "$state"}, zip_codes:{$addToSet: "$_id"}}},{$match:{zip_codes:{$size: 2}}},{$sort:{state:1 , _id:1}})
+
 
 4. Display the least populated city in each state
 
-R.  db.zips.aggregate({$group:{_id: {"city": "$city", "state": "$state"}, "city_pop": {$sum: "$pop"}}},{$sort: {city_pop : 1}},{$group:{_id: "$_id.state", city:{$first: "$_id.city"}, popupation: {$first: "$city_pop"}}},{$sort: {"_id":1}})
+R.  db.zips.aggregate({$group:{_id: {"city": "$city", "state": "$state"}, "city_pop": {$sum: "$pop"}}},
+{$sort: {city_pop : 1}},{$group:{_id: "$_id.state", city:{$first: "$_id.city"}, popupation: {$first: "$city_pop"}}},{$sort: {"_id":1}})
 
