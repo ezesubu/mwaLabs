33c3475e5c6fbd832fa34c691cd0c235baccec3e
Ezequiel Suarez <ezesubu@gmail.com>
2018-10-10 22:36:54 -0500

    change file

--- w2d3/exe.js
+++ w2d3/exe.js
@@ -2,8 +2,18 @@
 //mongoimport --db mwa -- file zips.json
 1.Find all the zip codes in Iowa state.
 
-db.zips.aggregate({$match: {population: {$min: 10000}}},{$group: {_id: "0",zip: {$addToSet: "$_id"}}} )
+R. db.zips.aggregate({$match: {population: {$min: 10000}}},{$group: {_id: "0",zip: {$addToSet: "$_id"}}} )
 
 2.Find all the zip codes with a population less than 1000.
 
-db.zips.aggregate({$match:{pop: {$lt:1000}},{$group: {_id: "0",zip: {$addToSet: "$_id"}}})
+R.  db.zips.aggregate({$match:{pop: {$lt:1000}}},{$group: {_id: "0",zip: {$addToSet: "$_id"}}})
+
+3.Find all cities that have more than one zip code, sort the results based by
+R.  state and city name.
+
+R. db.zips.aggregate({$group:{_id: "$city", zip_codes:{$addToSet: "$_id"}}},{$match:{zip_codes:{$size: 2}}})
+
+4. Display the least populated city in each state
+
+R.  db.zips.aggregate({$group:{_id: {"city": "$city", "state": "$state"}, "city_pop": {$sum: "$pop"}}},{$sort: {city_pop : 1}},{$group:{_id: "$_id.state", city:{$first: "$_id.city"}, popupation: {$first: "$city_pop"}}},{$sort: {"_id":1}})
+
